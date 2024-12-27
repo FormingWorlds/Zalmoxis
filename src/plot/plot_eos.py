c9c3816bdb2d5410c078e5511e4bc24634053def
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2024-12-27 14:00:57 +0100

    Update EOS reference, plotting adjustments

--- src/plot/plot_eos.py
+++ src/plot/plot_eos.py
@@ -5,12 +5,12 @@ import os
 # Function to read data from a file
 def read_eos_data(filename):
     data = np.loadtxt(filename, delimiter=',', skiprows=1)
-    pressure = data[:, 1]
-    density = data[:, 0] * 1000  # Convert to kg/m³
+    pressure = data[:, 1]  # Assuming pressure is in the second column (GPa)
+    density = data[:, 0] * 1000  # Assuming density is in the first column (g/cm³), convert to kg/m³
     return pressure, density
 
 # List of filenames
-data_files = ['eos_iron.txt', 'eos_silicate.txt', 'eos_water.txt']
+data_files = ['eos_seager07_iron.txt', 'eos_seager07_silicate.txt', 'eos_seager07_water.txt']
 data_folder = '../data/'
 
 # Create the plot
@@ -36,5 +36,14 @@ plt.yscale('log')
 # Invert the y-axis to make it downward-increasing
 plt.gca().invert_yaxis()
 
+# Add dotted horizontal helplines for the pressure in the center of the Earth and at the core-mantle boundary
+pressure_center_earth = 364  # Pressure in the center of the Earth in GPa
+pressure_cmb = 136  # Pressure at the core-mantle boundary in GPa
+
+plt.axhline(y=pressure_cmb, color='gray', linestyle=':', label="Earth's core-mantle boundary (136 GPa)")
+plt.axhline(y=pressure_center_earth, color='gray', linestyle='--', label="Earth's center (364 GPa)")
+
+
 # Show the plot
+plt.legend()
 plt.show()
