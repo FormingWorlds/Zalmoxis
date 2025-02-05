157155501e81a087bee42d471f669715d2480263
flaviacris03 <147712606+flaviacris03@users.noreply.github.com>
2025-02-05 11:52:05 +0100

    fixed conflicts in plot_eos

--- src/jord/plots/plot_eos.py
+++ src/jord/plots/plot_eos.py
@@ -22,34 +22,36 @@ def plot_eos_material(data_files, data_folder):
     The function assumes that the data files are located in the specified data_folder.
     The function plots the data on a log-log scale and inverts the y-axis to make it downward-increasing.
     """
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
 
     for file in data_files:
         filepath = os.path.join(data_folder, file)  # Use the passed data_folder
         pressure, density = read_eos_data(filepath)
         label = file.split('.')[0].replace('eos_', '').capitalize()
-        plt.plot(density, pressure, label=label)
+        ax.plot(density, pressure, label=label)
 
     # Set plot labels and title
-    plt.xlabel('Density (kg/m³)')
-    plt.ylabel('Pressure (GPa)')
-    plt.title('Equation of State Data')
-    plt.legend()
+    ax.set_xlabel('Density (kg/m³)')
+    ax.set_ylabel('Pressure (GPa)')
+    ax.set_title('Equation of State Data')
+    ax.legend()
 
     # Set log scale for both axes
-    plt.xscale('log')
-    plt.yscale('log')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
 
     # Invert the y-axis to make it downward-increasing
-    plt.gca().invert_yaxis()
+    ax.invert_yaxis()
 
     # Add dotted horizontal help lines for the pressure in the center of the Earth and at the core-mantle boundary
     pressure_center_earth = 364  # Pressure in the center of the Earth in GPa
     pressure_cmb = 136  # Pressure at the core-mantle boundary in GPa
 
-    plt.axhline(y=pressure_cmb, color='gray', linestyle=':', label="Earth's core-mantle boundary (136 GPa)")
-    plt.axhline(y=pressure_center_earth, color='gray', linestyle='--', label="Earth's center (364 GPa)")
+    ax.axhline(y=pressure_cmb, color='gray', linestyle=':', label="Earth's core-mantle boundary (136 GPa)")
+    ax.axhline(y=pressure_center_earth, color='gray', linestyle='--', label="Earth's center (364 GPa)")
 
     # Show the plot
-    plt.legend()
-    plt.savefig("planet_eos.png")
+    ax.legend()
+    fig.savefig("planet_eos.pdf")
+    #plt.show()
+    plt.close(fig)
