49995ae732e758fe2710ad6d99d6a9fd75c8489f
flaviacris03 <147712606+flaviacris03@users.noreply.github.com>
2025-02-05 12:16:26 +0100

    calling main minor change

--- src/jord/jord.py
+++ src/jord/jord.py
@@ -274,5 +274,4 @@ def main(temp_config_path=None, id_mass=None):
         plot_eos_material(eos_data_files, eos_data_folder)  # Call the EOS plotting function
         #plt.show()  # Show the plots
 
-if __name__ == "__main__":
-    main()
+
