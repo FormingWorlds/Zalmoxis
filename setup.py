ab9b93138a2c9a03a553d19af0206efa4103fd23
flaviacris03 <147712606+flaviacris03@users.noreply.github.com>
2025-03-16 23:26:56 +0100

    added folder structure in prep for documentation

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-project-name',          # Project name
+    version='0.1.0',                   # Version
+    packages=find_packages(),          # Automatically find all packages
+    install_requires=[                 # List core dependencies
+        'some-core-package',
+    ],
+    extras_require={                   # Define optional dependencies
+        'docs': [
+            'mkdocs',
+            'mkdocs-material',
+            'mkdocstrings',
+        ],
+    },
+    include_package_data=True,         # Optional: include other files like README.md, etc.
+)
